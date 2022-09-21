916a3fe9fd225686024acc78ad6e12c6212cd25d
222827 <112934203+222827@users.noreply.github.com>
2022-09-21 10:48:29 +0900

    Add files via upload

---
+++ chapter02/c_print.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+//제어문자
+// +\n :gkswnf rogod
+//+ \t : 탭\\\\(8칸 스페이스)
+
+int main(void)
+{
+    /*code*/
+     printf("HELLO\n");
+     print("Chulwoong Choi\t Choi");
+
+     return 0;
+ }
